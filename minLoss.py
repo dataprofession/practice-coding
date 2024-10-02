347f2a8366e0cfb91741c7a58ee121ab98b65ad1
dataprofessionallin <dataprofessional@yeah.net>
2024-10-02 03:12:32 +0000

    fix typo

--- minLoss.py
+++ minLoss.py
@@ -1,12 +1,12 @@
 # Minimum Loss Problem
 # Source: HackerRank - https://www.hackerrank.com/challenges/minimum-loss/problem
 # Solution:
-# The minLoss function sorts prices with their original indices and checks pairs to find the minimum loss \
+# The mininumLoss function sorts prices with their original indices and checks pairs to find the minimum loss \
 # by ensuring that the selling index is greater than the buying index. 
 # It focuses on comparing prices from the sorted list while keeping track of their original positions.
 
 import sys
-def minmumLoss(price):
+def minimumLoss(price):
     
     loss = sys.maxsize
         
@@ -29,12 +29,12 @@ class TestGetMoneySpent(unittest.TestCase):
     
     def test_case_1(self):
         price = [20,7,8,2,5]
-        result = minmumLoss(price)
+        result = minimumLoss(price)
         self.assertEqual(result, 2)
 
     def test_case_2(self):
         price = [2,3,4,1]
-        result = minmumLoss(price)
+        result = minimumLoss(price)
         self.assertEqual(result, 1)
 
 if __name__ == "__main__":
